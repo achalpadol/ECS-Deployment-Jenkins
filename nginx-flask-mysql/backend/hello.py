9c4c90aa3108ff4db6fe199a58f03ffe0e845b0a
Achal Padol <achalpadol@gmail.com>
2026-09-26 16:38:38 +0000

    file add

--- nginx-flask-mysql/backend/hello.py
+++ nginx-flask-mysql/backend/hello.py
@@ -4,46 +4,69 @@ import mysql.connector
 
 
 class DBManager:
-    def __init__(self, database='example', host="app1-db", user="root", password_file=None):
-        pf = open(password_file, 'r')
+    def __init__(self, database='example', host=None, user='root', password=None):
+
         self.connection = mysql.connector.connect(
-            user=user, 
-            password=pf.read(),
-            host=host, # name of the mysql service as set in the docker compose file
+            user=user,
+            password=password,
+            host=host,
             database=database,
             auth_plugin='mysql_native_password'
         )
-        pf.close()
+
         self.cursor = self.connection.cursor()
-    
+
     def populate_db(self):
         self.cursor.execute('DROP TABLE IF EXISTS blog')
-        self.cursor.execute('CREATE TABLE blog (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255))')
-        self.cursor.executemany('INSERT INTO blog (id, title) VALUES (%s, %s);', [(i, 'Blog post #%d'% i) for i in range (1,5)])
+
+        self.cursor.execute(
+            'CREATE TABLE blog (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255))'
+        )
+
+        self.cursor.executemany(
+            'INSERT INTO blog (id, title) VALUES (%s, %s);',
+            [(i, 'Blog post #%d' % i) for i in range(1, 5)]
+        )
+
         self.connection.commit()
-    
+
     def query_titles(self):
         self.cursor.execute('SELECT title FROM blog')
+
         rec = []
+
         for c in self.cursor:
             rec.append(c[0])
+
         return rec
 
 
 server = Flask(__name__)
+
 conn = None
+
 
 @server.route('/')
 def listBlog():
     global conn
+
     if not conn:
-        conn = DBManager(password_file='/run/secrets/db-password')
+        conn = DBManager(
+            database=os.getenv('MYSQL_DATABASE', 'example'),
+            host=os.getenv('MYSQL_HOST'),
+            user=os.getenv('MYSQL_USER', 'root'),
+            password=os.getenv('DB_PASSWORD')
+        )
+
         conn.populate_db()
+
     rec = conn.query_titles()
 
     response = ''
+
     for c in rec:
-        response = response  + '<div>   Hello  ' + c + '</div>'
+        response = response + '<div>Hello ' + c + '</div>'
+
     return response
 
 
